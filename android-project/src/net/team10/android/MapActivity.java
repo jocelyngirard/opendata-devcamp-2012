30c2ffc0fc11fac909a4e0a289f84603c4228654
w3m <s3r0m@hotmail.com>
2012-02-18 17:30:59 +0100

    Ajout MapActivity...

---
+++ android-project/src/net/team10/android/MapActivity.java
@@ -0,0 +1,29 @@
+package net.team10.android;
+
+import com.smartnsoft.droid4me.app.SmartMapActivity;
+
+public class MapActivity
+    extends SmartMapActivity
+{
+
+  public void onFulfillDisplayObjects()
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+  public void onRetrieveBusinessObjects()
+      throws BusinessObjectUnavailableException
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  protected String getGoogleMapsApiKey()
+  {
+    // TODO Auto-generated method stub
+    return "0yyOboPOlEVpjLWYbx-0ZU3Nn3HwjcLHrBvSLXg";
+  }
+
+}
