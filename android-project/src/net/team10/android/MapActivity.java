c76ec8cf4f4a5f85daf400662e8a06868885fef1
w3m <s3r0m@hotmail.com>
2012-02-18 23:56:23 +0100

    Add overlayManager.jar

--- android-project/src/net/team10/android/MapActivity.java
+++ android-project/src/net/team10/android/MapActivity.java
@@ -1,10 +1,25 @@
 package net.team10.android;
 
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.smartnsoft.droid4me.LifeCycle;
 import com.smartnsoft.droid4me.app.SmartMapActivity;
+
+import de.android1.overlaymanager.OverlayManager;
 
 public class MapActivity
     extends SmartMapActivity<TitleBar.TitleBarAggregate>
+    implements LifeCycle.BusinessObjectsRetrievalAsynchronousPolicy
 {
+
+  private MapView mapView;
+
+  private MapController mapController;
+
+  private OverlayManager overlayManager;
+
+  private MyLocationOverlay myLocationOverlay = null;
 
   @Override
   protected String getGoogleMapsApiKey()
@@ -20,5 +35,22 @@ public class MapActivity
 
   public void onFulfillDisplayObjects()
   {
+
+    mapView = getMapView();
+    mapView.setBuiltInZoomControls(true); // Display zoom for non-multitouch
+
+    // Géolocalisation
+    myLocationOverlay = new MyLocationOverlay(getApplicationContext(), mapView);
+    mapView.getOverlays().add(myLocationOverlay);
+    myLocationOverlay.enableMyLocation();
+
+    // Zoom to MyLocation
+    mapController = mapView.getController();
+    mapController.animateTo(myLocationOverlay.getMyLocation());
+    mapController.setZoom(15);
+
+    // OverlayManager
+    overlayManager = new OverlayManager(getApplication(), mapView);
+
   }
 }
