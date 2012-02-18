f90db698e793c7c0f50ddb2d6cc4999d08e4ced6
Jocelyn Girard <jocelyn@smartnsoft.com>
2012-02-19 00:16:21 +0100

    Add the POI reports list

--- android-project/src/net/team10/bo/Poi.java
+++ android-project/src/net/team10/bo/Poi.java
@@ -9,59 +9,69 @@ import org.codehaus.jackson.annotate.JsonProperty;
  * @since 2012.02.18
  */
 
-public class Poi implements Serializable {
-	
-	private String openDataTypeId;
-	
-	private String recordId;
-	
-	private String label;
-	
-	private String openDataGeo;
-	
-	private String openDataSetId;
-	
-	private String geoName;
+public class Poi
+    implements Serializable
+{
 
+  private static final long serialVersionUID = -7596551429335353456L;
 
-	public Poi(@JsonProperty("Info") String openDataTypeId, @JsonProperty("recordid") String recordId, @JsonProperty("Libelle")String label,
-			@JsonProperty("geom_x_y") String openDataGeo, @JsonProperty("geom_name") String geoName, @JsonProperty("datasetid") String openDataSetId) {
-		super();
-		this.openDataTypeId = openDataTypeId;
-		this.recordId = recordId;
-		this.label = label;
-		this.openDataGeo = openDataGeo;
-		this.openDataSetId = openDataSetId;
-	}
+  private final String openDataTypeId;
 
-	@JsonProperty("Info")
-	public String getOpenDataTypeId() {
-		return openDataTypeId;
-	}
-	
-	@JsonProperty("geom_name")
-	public String getGeoName() {
-		return geoName;
-	}
+  private final String recordId;
 
-	@JsonProperty("recordid")
-	public String getRecordId() {
-		return recordId;
-	}
+  private final String label;
 
-	@JsonProperty("Libelle")
-	public String getLabel() {
-		return label;
-	}
+  private final String openDataGeo;
 
-	@JsonProperty("geom_x_y")
-	public String getOpenDataGeo() {
-		return openDataGeo;
-	}
+  private final String openDataSetId;
 
-	@JsonProperty("datasetid")
-	public String getOpenDataSetId() {
-		return openDataSetId;
-	}
-	
+  private String geoName;
+
+  public Poi(@JsonProperty("Info") String openDataTypeId, @JsonProperty("recordid") String recordId, @JsonProperty("Libelle") String label,
+      @JsonProperty("geom_x_y") String openDataGeo, @JsonProperty("geom_name") String geoName, @JsonProperty("datasetid") String openDataSetId)
+  {
+    super();
+    this.openDataTypeId = openDataTypeId;
+    this.recordId = recordId;
+    this.label = label;
+    this.openDataGeo = openDataGeo;
+    this.openDataSetId = openDataSetId;
+  }
+
+  @JsonProperty("Info")
+  public String getOpenDataTypeId()
+  {
+    return openDataTypeId;
+  }
+
+  @JsonProperty("geom_name")
+  public String getGeoName()
+  {
+    return geoName;
+  }
+
+  @JsonProperty("recordid")
+  public String getRecordId()
+  {
+    return recordId;
+  }
+
+  @JsonProperty("Libelle")
+  public String getLabel()
+  {
+    return label;
+  }
+
+  @JsonProperty("geom_x_y")
+  public String getOpenDataGeo()
+  {
+    return openDataGeo;
+  }
+
+  @JsonProperty("datasetid")
+  public String getOpenDataSetId()
+  {
+    return openDataSetId;
+  }
+
 }
