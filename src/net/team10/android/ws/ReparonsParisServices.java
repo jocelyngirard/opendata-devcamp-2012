9832e0419a4e8c22f5919c10cbf4520d0dde0e12
Jocelyn Girard <jocelyn@smartnsoft.com>
2012-02-18 15:48:32 +0100

    Import the skeleton project

---
+++ src/net/team10/android/ws/ReparonsParisServices.java
@@ -0,0 +1,45 @@
+package net.team10.android.ws;
+
+import net.team10.android.Constants;
+
+import com.smartnsoft.droid4me.ws.WebServiceCaller;
+
+/**
+ * A single point of access to the web services.
+ * 
+ * @author Steeve Guillaume, Édouard Mercier, Willy Noel, Jocelyn Girard
+ * @since 2012.02.18
+ */
+public final class ReparonsParisServices
+    extends WebServiceCaller
+{
+
+  private static volatile ReparonsParisServices instance;
+
+  // We accept the "out-of-order writes" case
+  public static ReparonsParisServices getInstance()
+  {
+    if (instance == null)
+    {
+      synchronized (ReparonsParisServices.class)
+      {
+        if (instance == null)
+        {
+          instance = new ReparonsParisServices();
+        }
+      }
+    }
+    return instance;
+  }
+
+  private ReparonsParisServices()
+  {
+  }
+
+  @Override
+  protected String getUrlEncoding()
+  {
+    return Constants.WEBSERVICES_HTML_ENCODING;
+  }
+
+}
