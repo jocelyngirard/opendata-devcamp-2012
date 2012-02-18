56db47d8c84064975bb1a618e3abec77ae5d1eb8
Jocelyn Girard <jocelyn@smartnsoft.com>
2012-02-18 16:38:40 +0100

    Update with fragment support v4

--- android-project/src/net/team10/android/HomeActivity.java
+++ android-project/src/net/team10/android/HomeActivity.java
@@ -1,13 +1,6 @@
 package net.team10.android;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import android.content.Intent;
-
-import com.smartnsoft.droid4me.app.SmartActivity;
-import com.smartnsoft.droid4me.framework.Commands;
-import com.smartnsoft.droid4me.menu.StaticMenuCommand;
+import com.smartnsoft.droid4me.support.v4.app.SmartFragmentActivity;
 
 /**
  * The starting screen of the application.
@@ -16,56 +9,28 @@ import com.smartnsoft.droid4me.menu.StaticMenuCommand;
  * @since 2012.02.18
  */
 public final class HomeActivity
-    extends SmartActivity<TitleBar.TitleBarAggregate>
+    extends SmartFragmentActivity<Void>
 {
 
   public void onRetrieveDisplayObjects()
   {
-    setContentView(R.layout.home);
-    // TODO Auto-generated method stub
-
+    setContentView(R.layout.home_fragment);
   }
 
   public void onRetrieveBusinessObjects()
       throws BusinessObjectUnavailableException
   {
-    // TODO Auto-generated method stub
 
   }
 
   public void onFulfillDisplayObjects()
   {
-    // TODO Auto-generated method stub
 
   }
 
   public void onSynchronizeDisplayObjects()
   {
-    // TODO Auto-generated method stub
 
-  }
-
-  @Override
-  public List<StaticMenuCommand> getMenuCommands()
-  {
-    final List<StaticMenuCommand> commands = new ArrayList<StaticMenuCommand>();
-    commands.add(new StaticMenuCommand(R.string.Home_menu_settings, '1', 's', android.R.drawable.ic_menu_preferences, new Commands.StaticEnabledExecutable()
-    {
-      @Override
-      public void run()
-      {
-        startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
-      }
-    }));
-    commands.add(new StaticMenuCommand(R.string.Home_menu_about, '2', 'a', android.R.drawable.ic_menu_info_details, new Commands.StaticEnabledExecutable()
-    {
-      @Override
-      public void run()
-      {
-        startActivity(new Intent(getApplicationContext(), AboutActivity.class));
-      }
-    }));
-    return commands;
   }
 
 }
